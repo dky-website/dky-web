7ee985ad7c56ccb0da684c284887e66dfd683e26
ilongyou <962699676@qq.com>
2017-03-08 00:10:27 +0800

    add show param handle

--- routes/show.js
+++ routes/show.js
@@ -33,7 +33,32 @@ router.get(['/', '/:id'], function(req, res) {
             });
         });
     } else {
+        var jobs = [];
+        jobs.push(rocket.get(Constant.WEB_ROOT + '/front/banner/showBannerList'));
+        jobs.push(rocket.get(Constant.WEB_ROOT + '/front/show/showImageList?id=' + showId));
 
+        Promise.all([...jobs]).then(function([banResult, showResult]) {
+            var banResult = JSON.parse(banResult),
+                showResult = JSON.parse(showResult);
+
+            var bannerPics = banResult.data,
+                showData = showResult.data,
+                middleShow = showData.middleShow,
+                oldShowList = showData.oldShowList;
+
+            res.render('show', {
+                title: '旦可韵 - show',
+                menu: 'show',
+                bannerPics: bannerPics,
+                middleShow: middleShow,
+                oldShowList: oldShowList,
+                root: Constant.WEB_ROOT
+            });
+        }).catch(function(error) {
+            res.render('error', {
+                message: '抱歉，系统开小差了，请刷新重试.'
+            });
+        });
     }
 
 });
