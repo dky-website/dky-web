b68d1fd8f25a6c53e49c835b337fd2e02c731fcd
ilongyou <962699676@qq.com>
2017-03-11 00:25:59 +0800

    product page

--- routes/product.js
+++ routes/product.js
@@ -8,17 +8,23 @@ router.get('/', function(req, res) {
     var jobs = [];
     jobs.push(rocket.get(Constant.WEB_ROOT + '/front/product/queryProductType'));
     jobs.push(rocket.get(Constant.WEB_ROOT + '/front/product/querySeasonList'));
-    Promise.all([...jobs]).then(function([typeResult, seasonResult]) {
+    jobs.push(rocket.get(Constant.WEB_ROOT + '/front/product/queryProduct'));
+    Promise.all([...jobs]).then(function([typeResult, seasonResult, productResult]) {
         console.warn(typeResult, seasonResult, 'xxx')
         var typeResult = JSON.parse(typeResult),
-            seasonResult = JSON.parse(seasonResult);
-    });
-    res.render('product', {
-        title: '旦可韵 - product',
-        menu: 'product',
+            seasonResult = JSON.parse(seasonResult),
+            productResult= JSON.parse(productResult);
 
-        root: Constant.WEB_ROOT
+        res.render('product', {
+            title: '旦可韵 - product',
+            menu: 'product',
+            typeList: typeResult.data,
+            seasonList: seasonResult.data,
+            firstProduct: productResult.data,
+            root: Constant.WEB_ROOT
+        });
     });
+
 });
 
 module.exports = router;
