d955b7a1fa5595a70e4c18f8e6f0fc02121ad69b
ilongyou <962699676@qq.com>
2017-03-12 14:28:08 +0800

    complete articles

--- routes/article.js
+++ routes/article.js
@@ -5,8 +5,58 @@ var router = express.Router();
 
 /* GET common listing. */
 router.get('/:type', function(req, res) {
-    var type = req.params.type;
-    res.send('respond with a resource.' + type);
+    var type = req.params.type, typeName;
+
+    switch(type) {
+        case '1':
+            typeName = '关于我们';
+            break;
+        case '4':
+            typeName = '服务';
+            break;
+        case '5':
+            typeName = '联络';
+            break;
+        default:
+            return res.render('error', {
+                message: '不存在该菜单.'
+            });
+    }
+
+    var jobs = [];
+    jobs.push(rocket.get(Constant.WEB_ROOT + '/front/fmenu/list?type=' + type));
+
+    Promise.all([...jobs]).then(function([menuResult]) {
+        var menuResult = JSON.parse(menuResult);
+        if (menuResult.success) {
+            var menuList = menuResult.data;
+
+            if (menuList && menuList.length > 0) {
+                res.render('article', {
+                    title: '旦可韵 - ' + typeName,
+                    typeName: typeName,
+                    menu: 'article',
+                    menuList: menuList,
+                    menuListStr: JSON.stringify(menuList),
+                    root: Constant.WEB_ROOT
+                });
+            } else {
+                res.render('error', {
+                    message: '抱歉，' + typeName + '中暂时还未录入文章.'
+                });
+            }
+        } else {
+            res.render('error', {
+                message: '抱歉，文章信息获取失败.'
+            });
+        }
+
+
+    }).catch(function(error) {
+        res.render('error', {
+            message: '抱歉，系统开小差了，请刷新重试.'
+        });
+    });
 });
 
 module.exports = router;
