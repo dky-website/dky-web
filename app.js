a8588b904dedcde60f0fd55745245fee735ec7ac
ilongyou <962699676@qq.com>
2017-03-13 22:54:57 +0800

    动态生成footer

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ var product = require('./routes/product');
 var article = require('./routes/article');
 var users = require('./routes/user');
 
+var rocket = require('./utils/rocket');
+var Constant = require('./utils/const');
 var app = express();
 
 var env = process.env.NODE_ENV || 'development';
@@ -45,6 +47,99 @@ app.use(session({
 }));
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req, res, next) {
+    var jobs = [],
+        menuAbout = {},
+        menuProduct = {},
+        menuShow = {},
+        menuService = {},
+        menuContact = {};
+    menuAbout.title = '关于我们';
+    menuAbout.subMenu = [];
+    menuProduct.title = '产品';
+    menuProduct.subMenu = [];
+    menuShow.title = 'SHOW';
+    menuShow.subMenu = [];
+    menuService.title = '服务';
+    menuService.subMenu = [];
+    menuContact.title = '联络';
+    menuContact.subMenu = [];
+
+    jobs.push(rocket.get(Constant.WEB_ROOT + '/front/fmenu/list'));
+
+    Promise.all([...jobs]).then(function([menuResult]) {
+        var menuResult = JSON.parse(menuResult);
+        menuResult.data.map(function(menu, index) {
+            switch(menu.type) {
+                case '1': {
+                    // 关于我们
+                    menuAbout.subMenu.push({name: menu.menuName, url: '/article/1#' + menu.id})
+                    if (menu.hasChildren) {
+                        menu.children.map(function(subMenu, idx) {
+                            var url = '/article/1#' + subMenu.id;
+                            menuAbout.subMenu.push({name: subMenu.menuName, url: url})
+                        })
+                    }
+                }; break;
+                case '2': {
+                    // 产品
+                    menuProduct.subMenu.push({name: menu.menuName, url: '/product/' + menu.id})
+                    if (menu.hasChildren) {
+                        menu.children.map(function(subMenu, idx) {
+                            var url = '/product/' + subMenu.id;
+                            menuProduct.subMenu.push({name: subMenu.menuName, url: url})
+                        })
+                    }
+                }; break;
+                case '3': {
+                    // show
+                    menuShow.subMenu.push({name: menu.menuName, url: ('/show/' + menu.id)})
+                    if (menu.hasChildren) {
+                        menu.children.map(function(subMenu, idx) {
+                            menuShow.subMenu.push({name: subMenu.menuName, url: ('/show/' + subMenu.id)})
+                        })
+                    }
+                }; break;
+                case '4': {
+                    // 服务
+                    menuService.subMenu.push({name: menu.menuName, url: '/article/4#' + menu.id})
+                    if (menu.hasChildren) {
+                        menu.children.map(function(subMenu, idx) {
+                            var url = '/article/4#' + subMenu.id;
+                            menuService.subMenu.push({name: subMenu.menuName, url: url})
+                        })
+                    }
+                }; break;
+                case '5': {
+                    // 联络
+                    menuContact.subMenu.push({name: menu.menuName, url: '/article/5#' + menu.id})
+                    if (menu.hasChildren) {
+                        menu.children.map(function(subMenu, idx) {
+                            var url = '/article/5#' + subMenu.id;
+                            menuContact.subMenu.push({name: subMenu.menuName, url: url})
+                        })
+                    }
+                }; break;
+                default: ;
+            }
+        })
+        app.locals.menuAbout = menuAbout;
+        app.locals.menuProduct = menuProduct;
+        app.locals.menuShow = menuShow;
+        app.locals.menuService = menuService;
+        app.locals.menuContact = menuContact;
+        next();
+    }).catch(function(error) {
+        app.locals.menuAbout = menuAbout;
+        app.locals.menuProduct = menuProduct;
+        app.locals.menuShow = menuShow;
+        app.locals.menuService = menuService;
+        app.locals.menuContact = menuContact;
+        next();
+    });
+});
+
 
 app.use('/', routes);
 app.use('/show', show);
