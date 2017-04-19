6bc4ea8e8a0df60fc13be8f75490373e6e04ffc8
ilongyou <962699676@qq.com>
2017-04-19 09:22:37 +0800

    build product page

--- routes/product.js
+++ routes/product.js
@@ -6,18 +6,22 @@ var router = express.Router();
 /* GET product listing. */
 router.get(['/', '/:cnName'], function(req, res) {
     var jobs = [];
+    // jobs.push(rocket.get(Constant.WEB_ROOT + '/front/banner/productBannerList'));
+    jobs.push(rocket.get(Constant.WEB_ROOT + '/front/banner/showBannerList'));
     jobs.push(rocket.get(Constant.WEB_ROOT + '/front/product/queryProductType'));
     jobs.push(rocket.get(Constant.WEB_ROOT + '/front/product/querySeasonList'));
     jobs.push(rocket.get(Constant.WEB_ROOT + '/front/product/queryProduct'));
-    Promise.all([...jobs]).then(function([typeResult, seasonResult, productResult]) {
+    Promise.all([...jobs]).then(function([bannerResult, typeResult, seasonResult, productResult]) {
         //console.warn(typeResult, seasonResult, 'xxx')
-        var typeResult = JSON.parse(typeResult),
+        var bannerResult = JSON.parse(bannerResult),
+            typeResult = JSON.parse(typeResult),
             seasonResult = JSON.parse(seasonResult),
             productResult= JSON.parse(productResult);
-
+console.warn(bannerResult)
         res.render('product', {
             title: '旦可韵 - product',
             menu: 'product',
+            bannerPics: bannerResult.data,
             typeList: typeResult.data,
             seasonList: seasonResult.data,
             firstProduct: productResult.data,
